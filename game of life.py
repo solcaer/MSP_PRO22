2cdfba48af5ef583530091e4c2a2169d011ac9d6
AloeVeraKoala <68166147+AloeVeraKoala@users.noreply.github.com>
2022-06-07 18:06:48 +0200

    different size starting conditions
    added a thing to have different sized playing field

--- game of life.py
+++ game of life.py
@@ -30,59 +30,89 @@ def update (screen, cells, size, with_progress=False):
            pygame.draw.rect(screen,color,(col*size,row*size, size-1, size-1))
     return updated_cells
 
+
 def main ():
     pygame.init()
-    screen = pygame.display.set_mode((1000,800))
+    height= 1500
+    width = 1200
+    size= 15
+    h= int(height/size)
+    w= int(width/size)
+    screen = pygame.display.set_mode((height,width))
 
-    #cells = np.zeros((80,100))
-    cells = np.random.choice([1,0], 80*100, p=[0.15, 1-0.15]).reshape(80, 100)
+    cells = np.zeros((w,h))
+    #cells = np.random.choice([1,0], h*w, p=[0.15, 1-0.15]).reshape(w, h)
     screen.fill(Color_Grid)
-    update(screen,cells,10)
-
-    pygame.display.flip()
-    pygame.display.update()
-
+    update(screen,cells,size)
     running = False
+  
+    pygame.display.update()
+    #funtion to start with specific numbers of cells alive
+    def start_with_x (x):
+        REPLACE_COUNT = int(x)
+        REPLACE_WITH = 1
+        cells = np.zeros((w,h))
+        cells.flat[np.random.choice((h*w), REPLACE_COUNT, replace=False)] = REPLACE_WITH
+        return cells
+    
     #loop that runs all the time updating it without a pause
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+           #pauses when pressing space, continues when pressing it again
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = not running
-                    update (screen, cells, 10)
+                    update (screen, cells, size)
                     pygame.display.update()
+                    if running == True:
+                        print ("running.")
+                    elif running == False:
+                        print ("paused.")
+
 
                #when you click on a cell turns it into the opposite state
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                if cells [pos[1]//10,pos[0]//10]==1:
-                    cells [pos[1]//10,pos[0]//10]=0
-                    update (screen,cells,10)
+                if cells [pos[1]//size,pos[0]//size]==1:
+                    cells [pos[1]//size,pos[0]//size]=0
+                    update (screen,cells,size)
                     pygame.display.update()
+                    
                 else: 
-                    cells [pos[1]//10,pos[0]//10]=1
-                    update (screen,cells,10)
+                    cells [pos[1]//size,pos[0]//size]=1
+                    update (screen,cells,size)
                     pygame.display.update()
                #resets matrix and fills random spots with one, when pressing esc 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
-                    cells = np.random.choice([1,0], 80*100, p=[0.15, 1-0.15]).reshape(80, 100)
-                    update (screen, cells, 10)
+                    cells = np.random.choice([1,0], w*h, p=[0.15, 1-0.15]).reshape(w, h)
+                    update (screen, cells, size)
                     pygame.display.update()
-                #resets whole matrix to 0 when pressing R
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_R:
-                    running = False
-                    cells = cells = np.zeros((80,100))
-                    update (screen, cells, 10)
-                    pygame.display.update()
+                    print("reset random start")
+                    #resets whole matrix to 0 when pressing R
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        running = False
+                        cells = np.zeros((w,h))
+                        update (screen, cells, size)
+                        pygame.display.update()
+                        print ("cleared field")
+                        #random start with 500 cells alive
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_w:
+                            running = False
+                            cells= start_with_x(500)
+                            update (screen, cells, size)
+                            pygame.display.update()
+                            
+                            print("random start with x")
         screen.fill(Color_Grid)
         if running:
-            cells= update (screen, cells,10, with_progress=True)
+            cells= update (screen, cells,size, with_progress=True)
             pygame.display.update()
 
         time.sleep(0.05)
