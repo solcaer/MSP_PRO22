8665d6f7553f4b9e64cc8a9650dfbb9f40c32ddd
AloeVeraKoala <68166147+AloeVeraKoala@users.noreply.github.com>
2022-06-08 21:54:14 +0200

    game of life with specific time steps
    added a counter to only look at a specific number of generations

--- game of life.py
+++ game of life.py
@@ -1,41 +1,42 @@
+
 import numpy as np
 import time
 import pygame
 ##color values
 Color_BG=(10,10,10)
 Color_Grid = (40,40,40)
-Color_Die_Next =(170,170,170)
-Color_alive_next =(255,255,255)
+Color_Die_Next =(170,170,170) #color of cells that die the next generation  
+Color_alive_next =(255,255,255) # color of cells that turn alive next generation
 
-def update (screen, cells, size, with_progress=False):
+def update (screen, cells, size, with_progress=False): # rules and stuff to update
     updated_cells = np.zeros((cells.shape[0],cells.shape[1]))
     #goes through each cell and updates the state
     for row, col in np.ndindex(cells.shape):
-           alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells [row,col]
+           alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells [row,col] # checks cells around the cell
            color = Color_BG if cells[row, col] == 0 else Color_alive_next
 
-           if cells [row,col] == 1:
-               if alive < 2 or alive > 3:
+           if cells [row,col] == 1: #if the cell is alive do the following
+               if alive < 2 or alive > 3: #dies when less than 2 or more than 3 are around it
                    if with_progress:
                        color = Color_Die_Next
-               elif 2<= alive or alive<=3:
+               elif 2<= alive or alive<=3: #turns/stays alive when 2 or 3 cells around it are alive
                     updated_cells[row,col] = 1
                     if with_progress: 
                         color = Color_alive_next
-           else:
+           else: # if cell is dead do the following
                if alive == 3:
                    updated_cells[row,col] = 1
                    if with_progress:
                        color= Color_alive_next
-           pygame.draw.rect(screen,color,(col*size,row*size, size-1, size-1))
+           pygame.draw.rect(screen,color,(col*size,row*size, size-1, size-1)) # makes a grid the size of a matrix
     return updated_cells
 
 
 def main ():
-    pygame.init()
-    height= 800
-    width = 600
-    size= 15
+    pygame.init() # initiates the visual part
+    height= 1000 # some
+    width = 800 # variables to determine size of our field
+    size= 10
     h= int(height/size)
     w= int(width/size)
     screen = pygame.display.set_mode((height,width))
@@ -47,7 +48,7 @@ def main ():
     running = False
   
     pygame.display.update()
-    #funtion to start with specific numbers of cells alive
+    #function to make a matrix with specific numbers of cells alive at random locations
     def start_with_x (x):
         REPLACE_COUNT = int(x)
         REPLACE_WITH = 1
@@ -55,10 +56,18 @@ def main ():
         cells.flat[np.random.choice((h*w), REPLACE_COUNT, replace=False)] = REPLACE_WITH
         return cells
     
-    #loop that runs all the time updating it without a pause
+    # the following allows us only let the game go on for a specific amount of time
+    print ("please enter the number of timesteps")
+    timesteps= int(input())
+    x=timesteps
+    c=0 #counter to keep track of at what step we are
+    #game initiating loop
     while True:
+        
+        
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            
+            if event.type == pygame.QUIT: #makes us able to close the game
                 pygame.quit()
                 return
            #pauses when pressing space, continues when pressing it again
@@ -85,18 +94,22 @@ def main ():
                     cells [pos[1]//size,pos[0]//size]=1
                     update (screen,cells,size)
                     pygame.display.update()
-               #resets matrix and fills random spots with one, when pressing esc 
-            elif event.type == pygame.KEYDOWN:
+              
+            elif event.type == pygame.KEYDOWN: #resets matrix and fills random spots with one, when pressing esc 
                 if event.key == pygame.K_ESCAPE:
                     running = False
-                    cells = np.random.choice([1,0], w*h, p=[0.15, 1-0.15]).reshape(w, h)
+                    x = timesteps
+                    c = 0
+                    cells = np.random.choice([1,0], w*h, p=[0.15, 1-0.15]).reshape(w, h) # chance for a field to be alive is 15%
                     update (screen, cells, size)
                     pygame.display.update()
                     print("reset random start")
-                    #resets whole matrix to 0 when pressing R
-                elif event.type == pygame.KEYDOWN:
+                    
+                elif event.type == pygame.KEYDOWN:#resets whole matrix to 0(dead) when pressing R
                     if event.key == pygame.K_r:
                         running = False
+                        c=0
+                        x= timesteps
                         cells = np.zeros((w,h))
                         update (screen, cells, size)
                         pygame.display.update()
@@ -111,10 +124,54 @@ def main ():
                             
                             print("random start with x")
         screen.fill(Color_Grid)
-        if running:
+        if running and x>0:
+            x -= 1
+            c+=1 #counter to keep track of at what step we are
             cells= update (screen, cells,size, with_progress=True)
             pygame.display.update()
+        #once all timesteps done it stops so you can look at the end
+        elif x == 0:
+            running = False
+            update (screen, cells, size, with_progress=True)
+            pygame.display.update()
+            if c == timesteps: 
+                print(" total timesteps done:", c)
+            elif c > timesteps:
+                print ("another "+ str(timesteps) + " timesteps done")
+                print(" total timesteps done:", c)
+            print("stopped.")
+            #loop to "stop" the thing to look at current state after time steps
+            while True:
+                event = pygame.event.wait()
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        running = not running
+                        update (screen, cells, size)
+                        pygame.display.update()
+                        x = timesteps
+                        if running == True:
+                            print ("running.")
+                        elif running == False:
+                            print ("paused.")
+                            break
+                    elif event.key == pygame.K_r:
+                        running = False
+                        c=0
+                        x= timesteps
+                        cells = np.zeros((w,h))
+                        update (screen, cells, size)
+                        pygame.display.update()
+                        print ("cleared field")
+                        break
+            continue
+        
+                    
+            
 
         time.sleep(0.05)
+       
 if __name__== '__main__':
     main()
